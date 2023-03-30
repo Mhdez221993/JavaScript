2aa9df8ede698bcb9bdcc9579acda098b3b4093e
Moises Hernandez <mhdezcoronado@gmail.com>
2023-03-30 23:01:03 +0200

    Explain typed array

---
+++ 3.typesValuesVariables.js
@@ -0,0 +1,3 @@
+// typed array is an object that represents an array of binary data that has a fixed length and a specific type for its elements.
+let myArray = new Int8Array(4).fill(1);
+console.log(myArray);
