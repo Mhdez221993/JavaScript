2218efcd62776c13186299ba887770a676cde1d3
Moises Hernandez <mhdezcoronado@gmail.com>
2023-03-30 22:16:17 +0200

    Explain unicode normalization

--- lexicalStructure.js
+++ lexicalStructure.js
@@ -6,3 +6,13 @@ console.log(normalizedStr);
 let str2 = "Cafe\u0301";
 let normalizedStr2 = str2.normalize();
 console.log(normalizedStr2);
+
+let café = 1; // Define a variable using a Unicode character
+café; // => 1; access the variable using an escape sequence
+café; // => 1; another form of the same escape sequence
+
+console.log("\u{1F600}"); // Prints a smiley face emoji
+
+let a = 3,
+  b = 4;
+console.log(b);
