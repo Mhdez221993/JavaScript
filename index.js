4dabd0fc2143c9d9f80e0edebfa589ffbe1cc110
Moises Hernandez <mhdezcoronado@gmail.com>
2023-04-17 23:50:42 +0200

    cover corner casese for findPeakElement

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-function findMinRotated(arr) {
+function findPeakElement(arr) {
   let l = 0;
   let r = arr.length - 1;
 
-  while (l <= r) {
+  while (l < r) {
     let mid = Math.floor((l + r) / 2);
 
-    if (arr[mid] <= arr[r]) {
+    if (mid == arr.length - 1 || arr[mid] > arr[mid + 1]) {
       r = mid - 1;
     } else {
       l = mid + 1;
     }
   }
 
-  return l;
+  return arr[l];
 }
 
-let arr = [1, 2, 3, 5, 8];
-console.log(findMinRotated(arr));
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(findPeakElement(arr));
