801ec7c0d9ea7ae3fbcebd153d5e352f46245b70
Moises Hernandez <mhdezcoronado@gmail.com>
2023-04-18 01:10:39 +0200

    Find if n is perfec root

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-function firstElementNotSmallerThanTarget(arr, target) {
-  let l = 0;
-  let r = arr.length - 1;
+var isPerfectSquare = function (num) {
+  let min = 1;
+  let max = num;
 
-  while (l <= r) {
-    let mid = Math.floor((l + r) / 2);
+  while (min <= max) {
+    let mid = Math.floor((min + max) / 2);
 
-    if (arr[mid] < target) {
-      l = mid + 1;
+    if (mid * mid === num) {
+      return true;
+    } else if (mid * mid < num) {
+      min = mid + 1;
     } else {
-      r = mid - 1;
+      max = mid - 1;
     }
   }
 
-  return arr[l];
-}
-
-let arr = [1, 2, 3, 4, 5, 6];
-console.log(firstElementNotSmallerThanTarget(arr, 0));
+  return false;
+};
+console.log(isPerfectSquare(9));
