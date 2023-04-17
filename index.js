94204a448198de981dfff76245cc175440dc206f
Moises Hernandez <mhdezcoronado@gmail.com>
2023-04-18 00:53:28 +0200

    Find first element non smaller than target

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-function findPeakElement(arr) {
+function firstElementNotSmallerThanTarget(arr, target) {
   let l = 0;
   let r = arr.length - 1;
 
-  while (l < r) {
+  while (l <= r) {
     let mid = Math.floor((l + r) / 2);
 
-    if (mid == arr.length - 1 || arr[mid] > arr[mid + 1]) {
-      r = mid - 1;
-    } else {
+    if (arr[mid] < target) {
       l = mid + 1;
+    } else {
+      r = mid - 1;
     }
   }
 
   return arr[l];
 }
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(findPeakElement(arr));
+let arr = [1, 2, 3, 4, 5, 6];
+console.log(firstElementNotSmallerThanTarget(arr, 0));
