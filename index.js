f02b8a9d1d6dc831e4c7be48304f399d95ca5df2
Moises Hernandez <mhdezcoronado@gmail.com>
2023-04-17 22:01:16 +0200

    Find N occurrences of target in arr

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-function findFirstOccurrence(arr, target) {
+const readline = require("readline");
+
+function findNOccurrence(arr, target) {
   let l = 0;
   let r = arr.length - 1;
-  let firt_occurrence = -1;
 
   while (l <= r) {
     let mid = Math.floor((l + r) / 2);
 
-    if (arr[mid] === target && (mid === 0 || arr[mid - 1] !== target)) {
-      return mid;
-    }
-
-    if (arr[mid] === target) {
-      r = mid - 1;
-      firt_occurrence = mid;
-    } else if (arr[mid] > target) {
+    if (arr[mid] >= target) {
       r = mid - 1;
     } else {
       l = mid + 1;
     }
   }
 
-  return firt_occurrence;
+  if (arr[l] !== target) {
+    return -1;
+  }
+
+  let count = 0;
+  let i = l;
+  while (arr[i] && arr[i] === target) {
+    count++;
+    i++;
+  }
+
+  return count;
 }
+
+let arr = [1, 3, 3, 3, 3, 6, 10, 10, 10, 100];
+let target = 3;
+
+console.log(findFirstOccurrence(arr, target));
