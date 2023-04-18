eec710eb1eb500a6bee0cdb4b52e194c34396754
Moises Hernandez <mhdezcoronado@gmail.com>
2023-04-18 02:00:27 +0200

    Find minimun time to read news

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-var isPerfectSquare = function (num) {
-  let min = 1;
-  let max = num;
+function minimumTime(readTime, numWorkers) {
+  let l = Math.max(...readTime);
+  let r = readTime.reduce((a, b) => a + b, 0);
 
-  while (min <= max) {
-    let mid = Math.floor((min + max) / 2);
+  while (l < r) {
+    let mid = Math.floor((l + r) / 2);
+    let workers = 1;
+    let time = 0;
 
-    if (mid * mid === num) {
-      return true;
-    } else if (mid * mid < num) {
-      min = mid + 1;
+    for (let i = 0; i < readTime.length; i++) {
+      if (time + readTime[i] > mid) {
+        workers++;
+        time = 0;
+      }
+      time += readTime[i];
+    }
+
+    if (workers <= numWorkers) {
+      r = mid;
     } else {
-      max = mid - 1;
+      l = mid + 1;
     }
   }
 
-  return false;
-};
-console.log(isPerfectSquare(9));
+  return l;
+}
+
+let newspapers_read_times = [7, 2, 5, 10, 8];
+let num_coworkers = 2;
+
+console.log(minimumTime(newspapers_read_times, num_coworkers));
