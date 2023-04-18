b980f53dc8c841bde2a2a87ab9d9eb05e26924da
Moises Hernandez <mhdezcoronado@gmail.com>
2023-04-18 22:00:34 +0200

    Add even to calendar without doulbe booking

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-var minEatingSpeed = function (piles, h) {
-  let l = 1;
-  let r = 1000000000;
-  let ans = -1;
+var MyCalendar = function () {
+  this.calendar = [];
+};
 
-  while (l <= r) {
-    let mid = Math.floor((l + r) / 2);
+/**
+ * @param {number} start
+ * @param {number} end
+ * @return {boolean}
+ */
+MyCalendar.prototype.book = function (start, end) {
+  let [left, right, indx] = [0, this.calendar.length - 1, this.calendar.length];
 
-    if (can_finish_eating(piles, h, mid)) {
-      ans = mid;
-      r = mid - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (this.calendar[mid][0] > start) {
+      indx = mid;
+      right = mid - 1;
     } else {
-      l = mid + 1;
+      left = mid + 1;
     }
   }
 
-  return ans;
-};
-
-function can_finish_eating(piles, h, k) {
-  let hours_used = 0;
-
-  for (let pile of piles) {
-    hours_used += Math.ceil(pile / k);
+  if (
+    (indx > 0 && this.calendar[indx - 1][1] > start) ||
+    (indx < this.calendar.length && this.calendar[indx][0] < end)
+  ) {
+    return false;
   }
 
-  return hours_used <= h;
-}
+  this.calendar.splice(indx, 0, [start, end]);
 
-let piles = [3, 6, 7, 11];
-let h = 8;
-
-console.log(minEatingSpeed(piles, h));
+  return true;
+};
