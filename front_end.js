e35575d6b7f04b89650eee9da164cd223e1ba74e
Moises Hernandez <mhdezcoronado@gmail.com>
2023-04-26 23:53:04 +0200

    Implement bubble sort

--- front_end.js
+++ front_end.js
@@ -1,18 +1,27 @@
-function insertionSort(unsortedList) {
+function bubbleSort(unsortedList) {
   let arr = unsortedList;
+  let i = 0;
 
-  for (let i = 1; i < arr.length; i++) {
-    let curr = arr[i];
-    let j = i;
-
-    while (j > 0 && arr[j - 1] > arr[j]) {
-      [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
-      j--;
+  while (i < arr.length) {
+    let swapped = false;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+      console.log(i);
     }
+
+    if (!swapped) {
+      return arr;
+    }
+
+    i++;
   }
 
   return arr;
 }
 
-let arr = [5, 3, 1, 2, 4];
-console.log(insertionSort(arr));
+// let arr = [5, 3, 1, 2, 4];
+let arr = [1, 2, 3, 4, 5];
+console.log(bubbleSort(arr));
