30a798ff06e00697106a3245d6a55bf7af7878c7
Moises Hernandez <mhdezcoronado@gmail.com>
2023-04-27 00:26:03 +0200

    Implement merge sort

--- front_end.js
+++ front_end.js
@@ -1,27 +1,41 @@
-function bubbleSort(unsortedList) {
-  let arr = unsortedList;
-  let i = 0;
+function mergeSort(unsortedList) {
+  if (unsortedList.length <= 1) {
+    return unsortedList;
+  }
 
-  while (i < arr.length) {
-    let swapped = false;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > arr[i + 1]) {
-        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-        swapped = true;
-      }
-      console.log(i);
+  let mid = Math.floor(unsortedList.length / 2);
+  let left = mergeSort(unsortedList.slice(0, mid));
+  let right = mergeSort(unsortedList.slice(mid));
+
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  let [i, j] = [0, 0];
+  let result = [];
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
     }
+  }
 
-    if (!swapped) {
-      return arr;
-    }
-
+  while (i < left.length) {
+    result.push(left[i]);
     i++;
   }
 
-  return arr;
+  while (j < right.length) {
+    result.push(right[j]);
+    j++;
+  }
+
+  return result;
 }
 
-// let arr = [5, 3, 1, 2, 4];
-let arr = [1, 2, 3, 4, 5];
-console.log(bubbleSort(arr));
+let arr = [5, 3, 1, 2, 4];
+console.log(mergeSort(arr));
