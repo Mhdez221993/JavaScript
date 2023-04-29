ad2c3582fa3d4ffede481c93ddbfad3b140a8bcd
Moises Hernandez <mhdezcoronado@gmail.com>
2023-04-29 14:26:08 +0200

    Deserialize a binary tree

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-function treeHeight(tree) {
-  if (tree == null) return 0;
-  const leftHeight = treeHeight(tree.left);
-  const rightHeight = treeHeight(tree.right);
-  if (leftHeight === -1 || rightHeight === -1) return -1;
-  if (Math.abs(leftHeight - rightHeight) > 1) return -1;
-  return Math.max(leftHeight, rightHeight) + 1;
+class Node {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-function isBalanced(tree) {
-  return treeHeight(tree) !== -1;
+function dfs(nodes) {
+  const val = nodes.next().value;
+
+  if (val === "x") return null;
+
+  let newNode = new Node(parseInt(val, 10));
+
+  newNode.left = dfs(nodes);
+  newNode.right = dfs(nodes);
+
+  return newNode;
 }
+
+function deserialize(s) {
+  return dfs(s.split(" ")[Symbol.iterator]());
+}
+
+let s = "5 4 3 x x 8 x x 6 x x";
+console.log(deserialize(s));
