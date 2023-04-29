a6f1099ff8e8f1ff1c1e4f8af4f64a72e04f7431
Moises Hernandez <mhdezcoronado@gmail.com>
2023-04-29 17:42:46 +0200

    Invert a binary tree

--- index.js
+++ index.js
@@ -1,61 +1,9 @@
-class Node {
-  constructor(val, left = null, right = null) {
-    this.val = val;
-    this.left = left;
-    this.right = right;
-  }
+function invertBinaryTree(tree) {
+  if (!tree) return null;
+
+  return new Node(
+    tree.val,
+    invertBinaryTree(tree.right),
+    invertBinaryTree(tree.left)
+  );
 }
-
-function dfs(nodes) {
-  const val = nodes.next().value;
-
-  if (val === "x") return null;
-
-  let newNode = new Node(parseInt(val, 10));
-
-  newNode.left = dfs(nodes);
-  newNode.right = dfs(nodes);
-
-  return newNode;
-}
-
-function deserialize(s) {
-  return dfs(s.split(" ")[Symbol.iterator]());
-}
-
-// ------------------------------------------
-
-function serialize_dfs(root, res) {
-  if (!root) {
-    res.push("x");
-    return;
-  }
-
-  res.push(root.val);
-
-  serialize_dfs(root.left, res);
-  serialize_dfs(root.right, res);
-}
-
-function serialize(root) {
-  const res = [];
-  serialize_dfs(root, res);
-  return res.join(" ");
-}
-
-let s = "5 4 3 x x 8 x x 6 x x";
-const root = deserialize(s);
-
-// const serialized = serialize(root);
-
-function* print(root) {
-  if (!root) {
-    yield "x";
-  } else {
-    yield root.val;
-    yield* print(root.left);
-    yield* print(root.right);
-  }
-}
-
-console.log(Array.from(print(root)));
