2f4431948217b6bf38fc93c039974a1b8b4523ce
Moises Hernandez <mhdezcoronado@gmail.com>
2023-04-29 17:15:23 +0200

    Serialiazing and deserializing binary tree

--- index.js
+++ index.js
@@ -23,5 +23,27 @@ function deserialize(s) {
   return dfs(s.split(" ")[Symbol.iterator]());
 }
 
+// ------------------------------------------
+
+function serialize_dfs(root, res) {
+  if (!root) {
+    res.push("x");
+    return;
+  }
+
+  res.push(root.val);
+
+  serialize_dfs(root.left, res);
+  serialize_dfs(root.right, res);
+}
+
+function serialize(root) {
+  const res = [];
+  serialize_dfs(root, res);
+  return res.join(" ");
+}
+
 let s = "5 4 3 x x 8 x x 6 x x";
-console.log(deserialize(s));
+let root = deserialize(s);
+
+console.log(serialize(root));
