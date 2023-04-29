24f888cffab63db7d808d6da6ac6e5ea3bee84df
Moises Hernandez <mhdezcoronado@gmail.com>
2023-04-29 14:06:30 +0200

    Check if a tree is balanced

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-function visibleTreeNode(root, max = -Infinity) {
-  if (!root) return 0;
+function treeHeight(tree) {
+  if (tree == null) return 0;
+  const leftHeight = treeHeight(tree.left);
+  const rightHeight = treeHeight(tree.right);
+  if (leftHeight === -1 || rightHeight === -1) return -1;
+  if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+  return Math.max(leftHeight, rightHeight) + 1;
+}
 
-  let total = 0;
-  if (root.val >= max) total++;
-
-  total += visibleTreeNode(root, Math.max(root.left, Math.max(root.val, max)));
-  total += visibleTreeNode(root, Math.max(root.right, Math.max(root.val, max)));
-
-  return total;
+function isBalanced(tree) {
+  return treeHeight(tree) !== -1;
 }
