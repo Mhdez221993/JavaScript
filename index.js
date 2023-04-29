2c1539b11b0d3a362e9dace2f00bbe7d905dbbcd
Moises Hernandez <mhdezcoronado@gmail.com>
2023-04-29 13:15:05 +0200

    Implement visible tree nodes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function treeMaxDepth(root) {
-  return !root ? 0 : dfs(root) - 1;
-}
-
-function dfs(root) {
+function visibleTreeNode(root, max = -Infinity) {
   if (!root) return 0;
 
-  return Math.max(dfs(root.left), dfs(root.right)) + 1;
+  let total = 0;
+  if (root.val >= max) total++;
+
+  total += visibleTreeNode(root, Math.max(root.left, Math.max(root.val, max)));
+  total += visibleTreeNode(root, Math.max(root.right, Math.max(root.val, max)));
+
+  return total;
 }
