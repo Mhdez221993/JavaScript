13455e9567512b53e8930b8bb1a0b4069079ff90
Moises Hernandez <mhdezcoronado@gmail.com>
2023-04-30 16:03:51 +0200

    Implement delete method for BST

--- index.js
+++ index.js
@@ -30,6 +30,46 @@ class BST {
     return tree;
   }
 
+  find(val, tree = this.root, parent = this.root, is_left = false) {
+    if (!tree) {
+      return { found: false };
+    }
+
+    if (val === tree.val) {
+      return { found: true, parent, node: tree, is_left };
+    } else if (val < tree.val) {
+      return this.find(val, tree.left, tree, true);
+    } else {
+      return this.find(val, tree.right, tree, false);
+    }
+  }
+
+  delete(val, tree = this.root) {
+    let { found, parent, node, is_left } = this.find(val, tree);
+    if (!found) return false;
+
+    if (!node.right) {
+      if (is_left) {
+        parent.left = node.left;
+      } else {
+        parent.right = node.left;
+      }
+    } else {
+      let min_node = this.min(node.right);
+      // console.log(node.right);
+      node.val = min_node;
+      return this.delete(min_node, node.right);
+    }
+  }
+
+  min(tree = this.root) {
+    if (!tree.left) {
+      return tree.val;
+    }
+
+    return this.min(tree.left);
+  }
+
   print() {
     console.log(this.root);
   }
@@ -41,5 +81,9 @@ let bst = new BST();
 for (let v of arr) {
   bst.insert(v);
 }
+
+// console.log(bst.find(4).found);
+
+bst.delete(10);
 
 bst.print();
