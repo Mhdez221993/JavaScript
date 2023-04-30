36cb1d015bc6d12c79a09f91cc19f74d7fd55a79
Moises Hernandez <mhdezcoronado@gmail.com>
2023-04-30 17:56:24 +0200

    Check if BST is vaid

--- index.js
+++ index.js
@@ -6,84 +6,13 @@ class Node {
   }
 }
 
-class BST {
-  constructor() {
-    this.root = null;
-  }
+function validBst(root, min = -Infinity, max = Infinity) {
+  if (!root) return true;
 
-  insert(val, tree = this.root) {
-    if (!this.root) {
-      this.root = new Node(val);
-      return;
-    }
+  if (!(min < root.val && root.val < max)) return false;
 
-    if (!tree) {
-      return new Node(val);
-    }
+  let left = validBst(root.left, min, root.val);
+  let right = validBst(root.left, root.val, max);
 
-    if (val < tree.val) {
-      tree.left = this.insert(val, tree.left);
-    } else {
-      tree.right = this.insert(val, tree.right);
-    }
-
-    return tree;
-  }
-
-  find(val, tree = this.root, parent = this.root, is_left = false) {
-    if (!tree) {
-      return { found: false };
-    }
-
-    if (val === tree.val) {
-      return { found: true, parent, node: tree, is_left };
-    } else if (val < tree.val) {
-      return this.find(val, tree.left, tree, true);
-    } else {
-      return this.find(val, tree.right, tree, false);
-    }
-  }
-
-  delete(val, tree = this.root) {
-    let { found, parent, node, is_left } = this.find(val, tree);
-    if (!found) return false;
-
-    if (!node.right) {
-      if (is_left) {
-        parent.left = node.left;
-      } else {
-        parent.right = node.left;
-      }
-    } else {
-      let min_node = this.min(node.right);
-      // console.log(node.right);
-      node.val = min_node;
-      return this.delete(min_node, node.right);
-    }
-  }
-
-  min(tree = this.root) {
-    if (!tree.left) {
-      return tree.val;
-    }
-
-    return this.min(tree.left);
-  }
-
-  print() {
-    console.log(this.root);
-  }
+  return left && right;
 }
-
-let arr = [8, 3, 10, 1, 6, 4];
-
-let bst = new BST();
-for (let v of arr) {
-  bst.insert(v);
-}
-
-// console.log(bst.find(4).found);
-
-bst.delete(10);
-
-bst.print();
