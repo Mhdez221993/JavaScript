37d83d9d04b93b63695e6f2c13d6b7afc6a84aeb
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-01 01:01:55 +0200

    Find the lowest common anssesstor in a BT

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
+function lca(root, node1, node2) {
+  if (!root) return;
+
+  if (root === node1 || root === node2) return root;
+
+  let left = lca(root.left, node1, node2);
+  let right = lca(root.right, node1, node2);
+
+  if (left && right) return root;
+
+  return left || right;
 }
 
-function lcaOnBst(bst, p, q) {
-  if (!bst) return null;
+let root = {
+  val: 6,
+  left: {
+    val: 4,
+    left: { val: 3, left: null, right: null },
+    right: { val: 5, left: null, right: null },
+  },
+  right: { val: 8, left: null, right: null },
+};
 
-  if (bst.val < p && bst.val < q) {
-    return lcaOnBst(bst.right, p, q);
-  } else if (bst.val > p && bst.val > q) {
-    return lcaOnBst(bst.left, p, q);
-  } else {
-    return bst.val;
-  }
-}
+let node1 = root.left.left;
+let node2 = root.left.right;
+
+console.log(lca(root, node1, node2));
