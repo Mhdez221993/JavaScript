9ab6b1e31d354d2bd0e4089465b74acb20acd216
Moises Hernandez <mhdezcoronado@gmail.com>
2023-04-30 14:43:32 +0200

    Implement insert method in BST

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-function invertBinaryTree(tree) {
-  if (!tree) return null;
-
-  return new Node(
-    tree.val,
-    invertBinaryTree(tree.right),
-    invertBinaryTree(tree.left)
-  );
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
 }
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val, tree = this.root) {
+    if (!this.root) {
+      this.root = new Node(val);
+      return;
+    }
+
+    if (!tree) {
+      return new Node(val);
+    }
+
+    if (val < tree.val) {
+      tree.left = this.insert(val, tree.left);
+    } else {
+      tree.right = this.insert(val, tree.right);
+    }
+
+    return tree;
+  }
+
+  print() {
+    console.log(this.root);
+  }
+}
+
+let arr = [8, 3, 10, 1, 6, 4];
+
+let bst = new BST();
+for (let v of arr) {
+  bst.insert(v);
+}
+
+bst.print();
