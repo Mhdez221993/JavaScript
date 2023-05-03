d5e02acc02739f970945b07e28103ef02830db77
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-04 01:38:54 +0200

    Find the nth fib number

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
-function permutation(nums) {
-  let res = [];
-  let used = Array(nums.length).fill(false);
-
-  function dfs(startIndex, path, res, used) {
-    if (startIndex == nums.length) {
-      res.push(path.join(""));
-      return;
-    }
-
-    for (let i = 0; i < nums.length; i++) {
-      if (!used[i]) {
-        used[i] = true;
-        path.push(nums[i]);
-        dfs(startIndex + 1, path, res, used);
-        path.pop();
-        used[i] = false;
-      }
-    }
+function fib(n) {
+  if (n === 0 || n === 1) {
+    return n;
   }
 
-  dfs(0, [], res, used);
-  return res;
+  return fib(n - 1) + fib(n - 2);
 }
 
-let nums = [1, 2, 3];
-console.log(permutation(nums));
+console.log(fib(5));
