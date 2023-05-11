a7494244f6ec21800f45e85b1a11f7648f16a75f
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-11 16:39:26 +0200

    Implement a different function

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-function compact(array) {
-  return array.filter(Boolean);
+function difference(array, values) {
+  const result = [];
+
+  // Create a set of all the values in the values arrays
+  const valuesSet = new Set(values.flat());
+
+  for (let i = 0; i < array.length; i++) {
+    const value = array[i];
+    // Check if the value is in the values set
+    if (!valuesSet.has(value) && !(value === undefined && !(i in array))) {
+      result.push(value);
+    }
+  }
+
+  return result;
 }
 
-console.log(compact([0, 1, false, 2, "", 3, null])); // => [1, 2, 3]
-console.log(compact(["hello", 123, [], {}, function () {}])); // => ['hello', 123, [], {}, function() {}]
+console.log(difference([1, 2, 3], [2, 3])); // => [1]
+console.log(difference([1, 2, 3, 4], [2, 3], [1, 2])); // => [4]
+console.log(difference([1, 2, 3], [2, 3], [1, 4])); // => []
+console.log(difference([1, , 3], [1])); // => [3]
+
+console.log(difference([1, 2, 3], [])); // => [1, 2, 3]
+
+console.log(difference([1, undefined, 3, 4], [3, 4]));
