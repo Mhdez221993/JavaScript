7f3161e3c29a4eace0d50428250a14da27f34daa
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-11 12:24:15 +0200

    Add backtracking with pruning template

--- 3.backtracking.js
+++ 3.backtracking.js
@@ -1,14 +1,33 @@
 // Back tracking template
-
 function dfs(start_index, path) {
-  is_leaf(start_index) {
-    report(path)
-    return
+  if (is_leaf(start_index)) {
+    report(path);
+    return;
   }
 
-  for (edge of get_edges(start_index)){
-    path.add(edge)
-    dfs(start_index + 1, path)
-    path.pop()
+  for (edge of get_edges(start_index)) {
+    path.add(edge);
+    dfs(start_index + 1, path);
+    path.pop();
+  }
+}
+
+// Backtracking with Pruning template
+function dfs(start_index, path) {
+  if (is_leaf(start_index)) {
+    report(path);
+    return;
+  }
+
+  for (let edge of get_edges(start_index)) {
+    // prune if needed
+    if (is_not_valid(edge)) {
+      continue;
+    }
+
+    path.add(edge);
+    // increment start_index
+    dfs(start_index + len(edge), path);
+    path.pop();
   }
 }
