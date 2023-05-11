74600284aef72e707dd05e39562cb37a59cc0173
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-11 11:25:49 +0200

    Combine the letterCombination fucntion with dfs

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-function lettersCombination(n) {
+function letterCombination(n) {
   let res = [];
-  dfs(n, [], res);
+
+  function dfs(path) {
+    if (path.length === n) {
+      res.push(path.join(""));
+      return;
+    }
+
+    for (let char of "ab") {
+      path.push(char);
+      dfs(path);
+      path.pop();
+    }
+  }
+
+  dfs([]);
   return res;
 }
 
-function dfs(n, path, res) {
-  if (path.length === n) {
-    res.push(path.join());
-    return;
-  }
-
-  for (let char of "ab") {
-    path.push(char);
-    dfs(n, path, res);
-    path.pop();
-  }
-}
-
-console.log(lettersCombination(2));
+console.log(letterCombination(2));
