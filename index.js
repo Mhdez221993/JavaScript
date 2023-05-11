fe28296217c23dbba16566c06b64653e33ff2367
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-11 15:16:46 +0200

    Implement function chunk that splits the input into groups of length size

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-function clamp(value, lower, upper) {
-  if (value < lower) {
-    return lower;
-  } else if (value > upper) {
-    return upper;
-  } else {
-    return value;
+function chunk(array, size = 1) {
+  let count = 0;
+  let result = [];
+  let chuck = [];
+
+  for (let i = 0; i < array.length; i++) {
+    chuck.push(array[i]);
+    count++;
+
+    if (count === size || i === array.length - 1) {
+      count = 0;
+      result.push([...chuck]);
+      chuck = [];
+    }
   }
+
+  return result;
 }
 
-console.log(clamp(-10, -5, 5));
-console.log(clamp(10, -5, 5));
-console.log(clamp(1, -5, 5));
+console.log(chunk(["a", "b", "c", "d"])); // => [['a'], ['b'], ['c'], ['d']]
+console.log(chunk([1, 2, 3, 4], 2)); // => [[1, 2], [3, 4]]
+console.log(chunk([1, 2, 3, 4], 3)); // => [[1, 2, 3], [4]]
