02347dc940f09d885b654fbf6eba85f498c89db4
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-11 16:50:31 +0200

    Implement drop right function

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-function difference(array, values) {
-  const result = [];
-
-  // Create a set of all the values in the values arrays
-  const valuesSet = new Set(values.flat());
-
-  for (let i = 0; i < array.length; i++) {
-    const value = array[i];
-    // Check if the value is in the values set
-    if (!valuesSet.has(value) && !(value === undefined && !(i in array))) {
-      result.push(value);
-    }
-  }
-
-  return result;
+function dropRightWhile(array, predicate) {
+  return array.filter((v) => !predicate(v));
 }
 
-console.log(difference([1, 2, 3], [2, 3])); // => [1]
-console.log(difference([1, 2, 3, 4], [2, 3], [1, 2])); // => [4]
-console.log(difference([1, 2, 3], [2, 3], [1, 4])); // => []
-console.log(difference([1, , 3], [1])); // => [3]
-
-console.log(difference([1, 2, 3], [])); // => [1, 2, 3]
-
-console.log(difference([1, undefined, 3, 4], [3, 4]));
+console.log(dropRightWhile([1, 2, 3, 4, 5], (value) => value > 3)); // => [1, 2, 3]
+console.log(dropRightWhile([1, 2, 3], (value) => value < 6)); // => []
+console.log(dropRightWhile([1, 2, 3, 4, 5], (value) => value > 6)); // => [1, 2, 3, 4, 5]
