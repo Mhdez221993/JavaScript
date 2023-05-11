53384fd07997731e4f9c02978590eb2f7e9f3305
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-11 11:40:41 +0200

    Generate All Letter Combinations from a Phone Number

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-function letterCombination(n) {
+function letterCombinationsOfPhoneNumber(digits) {
   let res = [];
-
-  function dfs(path, startIndex) {
-    if (startIndex === n) {
-      res.push(path.join(""));
-      return;
-    }
-
-    for (let char of "ab") {
-      path.push(char);
-      dfs(path, startIndex + 1);
-      path.pop();
-    }
-  }
-
-  dfs([], 0);
+  dfs(digits, 0, [], res);
   return res;
 }
 
-console.log(letterCombination(2));
+function dfs(digits, startIndex, path, res) {
+  if (startIndex === digits.length) {
+    res.push(path.join(""));
+    return;
+  }
+
+  for (let char of KEYBOARD[digits[startIndex]]) {
+    path.push(char);
+    dfs(digits, startIndex + 1, path, res);
+    path.pop();
+  }
+}
+
+const KEYBOARD = {
+  2: "abc",
+  3: "def",
+  4: "ghi",
+  5: "jkl",
+  6: "mno",
+  7: "pqrs",
+  8: "tuv",
+  9: "wxyz",
+};
+
+const digits = "56";
+
+console.log(letterCombinationsOfPhoneNumber(digits));
