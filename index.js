2848f606cd2cbf7e4c8ddd5e018e304e132b9542
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-12 17:59:02 +0200

    Implement a Stack with a linked list

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.prev = null;
+  }
+}
+
 class Stack {
   constructor() {
-    this.stack = [];
+    this.top = null;
+    this.count = 0;
   }
 
   /**
@@ -9,8 +17,14 @@ class Stack {
    * @return {number} The new length of the stack.
    */
   push(item) {
-    this.stack.push(item);
-    return this.length();
+    const newNode = new Node(item);
+
+    newNode.prev = this.top;
+    this.top = newNode;
+
+    this.count += 1;
+
+    return this.count;
   }
 
   /**
@@ -20,7 +34,12 @@ class Stack {
   pop() {
     if (this.isEmpty()) return undefined;
 
-    return this.stack.pop();
+    const node = this.top;
+    this.top = this.top.prev;
+
+    this.count -= 1;
+
+    return node.val;
   }
 
   /**
@@ -28,7 +47,7 @@ class Stack {
    * @return {boolean} `true` if the stack has no items, `false` otherwise.
    */
   isEmpty() {
-    return this.stack.length === 0;
+    return this.count === 0;
   }
 
   /**
@@ -36,7 +55,9 @@ class Stack {
    * @return {*} The item at the top of the stack if it is not empty, `undefined` otherwise.
    */
   peek() {
-    return this.isEmpty() ? undefined : this.stack[this.stack.length - 1];
+    if (this.isEmpty()) return undefined;
+
+    return this.top.val;
   }
 
   /**
@@ -44,9 +65,10 @@ class Stack {
    * @return {number} The number of items in the stack.
    */
   length() {
-    return this.stack.length;
+    return this.count;
   }
 }
 
 const s = new Stack();
-console.log(s.isEmpty());
+console.log(s);
+console.log(s.length());
