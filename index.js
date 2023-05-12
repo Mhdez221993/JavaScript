aa86640145c409d7723354a8c171c0e11a71a630
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-12 12:05:16 +0200

    Improve generate permutaion function

--- index.js
+++ index.js
@@ -1,67 +1,26 @@
-function generateParentheses(n) {
+function permutations(letters) {
   let res = [];
-
-  function dfs(path, openCount, closeCount) {
-    if (path.length === n * 2) {
-      res.push(path.join(""));
-      return;
-    }
-
-    for (let char of ["(", ")"]) {
-      if (char === "(" && openCount >= n) {
-        continue;
-      }
-
-      if (char === ")" && closeCount >= openCount) {
-        continue;
-      }
-
-      if (char === "(") {
-        openCount += 1;
-      } else {
-        closeCount += 1;
-      }
-
-      path.push(char);
-      dfs(path, openCount, closeCount);
-
-      if (char === "(") {
-        openCount -= 1;
-      } else {
-        closeCount -= 1;
-      }
-
-      path.pop();
-    }
-  }
-
-  dfs([], 0, 0);
+  let used = Array(letters.length).fill(false);
+  dfs(letters, used, res, []);
   return res;
 }
 
-function* main() {
-  const n = parseInt(yield);
-  const res = generateParentheses(n);
-  res.sort();
-  for (const line of res) {
-    console.log(line);
+function dfs(letters, used, res, path) {
+  if (path.length === letters.length) {
+    res.push(path.join(""));
+    return;
+  }
+
+  for (let i = 0; i < letters.length; i++) {
+    if (!used[i]) {
+      used[i] = letters[i];
+      path.push(letters[i]);
+      dfs(letters, used, res, path);
+      path.pop();
+      used[i] = false;
+    }
   }
 }
 
-class EOFError extends Error {}
-{
-  const gen = main();
-  const next = (line) => gen.next(line).done && process.exit();
-  let buf = "";
-  next();
-  process.stdin.setEncoding("utf8");
-  process.stdin.on("data", (data) => {
-    const lines = (buf + data).split("\n");
-    buf = lines.pop();
-    lines.forEach(next);
-  });
-  process.stdin.on("end", () => {
-    buf && next(buf);
-    gen.throw(new EOFError());
-  });
-}
+let letters = "abc";
+console.log(permutations(letters));
