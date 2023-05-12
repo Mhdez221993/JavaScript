3dad32a32bcca23590d4bfeaafa8577e2c8816bf
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-12 17:19:58 +0200

    Implement a stack array-base solucion

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
-function inRange(value, startParam, endParam) {
-  let start = startParam;
-  let end = endParam;
-  if (end === undefined) {
-    end = start;
-    start = 0;
+class Stack {
+  constructor() {
+    this.stack = [];
   }
 
-  if (start < end) {
-    return value >= start && value < end;
+  /**
+   * Pushes an item onto the top of the stack.
+   * @param {*} item The item to be pushed onto the stack.
+   * @return {number} The new length of the stack.
+   */
+  push(item) {
+    this.stack.push(item);
+    return this.length();
   }
 
-  return value >= end && value < start;
+  /**
+   * Remove an item at the top of the stack.
+   * @return {*} The item at the top of the stack if it is not empty, `undefined` otherwise.
+   */
+  pop() {
+    if (this.isEmpty()) return undefined;
+
+    return this.stack.pop();
+  }
+
+  /**
+   * Determines if the stack is empty.
+   * @return {boolean} `true` if the stack has no items, `false` otherwise.
+   */
+  isEmpty() {
+    return this.stack.length === 0;
+  }
+
+  /**
+   * Returns the item at the top of the stack without removing it from the stack.
+   * @return {*} The item at the top of the stack if it is not empty, `undefined` otherwise.
+   */
+  peek() {
+    return this.isEmpty() ? undefined : this.stack[this.stack.length - 1];
+  }
+
+  /**
+   * Returns the number of items in the stack.
+   * @return {number} The number of items in the stack.
+   */
+  length() {
+    return this.stack.length;
+  }
 }
 
-function inRange(value, start, end) {
-  if (!end) {
-    end = start;
-    start = 0;
-  }
-
-  return value >= Math.min(start, end) && value < Math.max(start, end);
-}
+const s = new Stack();
+console.log(s.isEmpty());
