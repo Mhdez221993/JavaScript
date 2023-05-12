262b1e3bc3a17c342262e6437d8e2f094d3a6f84
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-12 22:58:17 +0200

    Create a program that print nxn str

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function uniqueArray(array) {
-  return Array.from(new Set(array));
+// Your code here.
+let size = 10;
+let char = "#";
+let str = "";
+
+for (let i = 0; i < size - 1; i++) {
+  char += " #";
 }
 
-console.log(uniqueArray([1, 2, 3, 3, 3, null, undefined]));
+for (let i = 0; i < size; i++) {
+  if (i % 2 === 0) str = `${str}\n ${char}`;
+  if (i % 2 !== 0) str = `${str}\n${char}`;
+}
+
+console.log(str);
