a7c2d4464d9810d662e8e072d44afc7baf1f7bde
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-12 12:13:05 +0200

    Improve code redability in permutaion function

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 function permutations(letters) {
   let res = [];
-  let used = Array(letters.length).fill(false);
-  dfs(letters, used, res, []);
-  return res;
-}
 
-function dfs(letters, used, res, path) {
-  if (path.length === letters.length) {
-    res.push(path.join(""));
-    return;
-  }
+  function dfs(path, used) {
+    if (path.length === letters.length) {
+      res.push(path.join(""));
+      return;
+    }
 
-  for (let i = 0; i < letters.length; i++) {
-    if (!used[i]) {
-      used[i] = letters[i];
-      path.push(letters[i]);
-      dfs(letters, used, res, path);
-      path.pop();
-      used[i] = false;
+    for (let i = 0; i < letters.length; i++) {
+      if (!used[i]) {
+        used[i] = true;
+        path.push(letters[i]);
+        dfs(path, used);
+        path.pop();
+        used[i] = false;
+      }
     }
   }
+
+  dfs([], Array(letters.length).fill(false));
+  return res;
 }
 
 let letters = "abc";
