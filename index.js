3623be666106a6b22c8edf48aefcb420e6ebc9f0
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-13 23:01:55 +0200

    Generate All Valid Parentheses

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-function partition(s) {
-  let res = [];
-  dfs(s, 0, [], res);
-  return res;
-}
+function generateParentheses(n) {
+  const dfs = (path, openCount, closeCount, res) => {
+    if (path.length === 2 * n) {
+      res.push(path.join(""));
+      return;
+    }
 
-function dfs(s, start, path, res) {
-  if (start === s.length) {
-    res.push([...path]);
-    return;
-  }
+    for (const parenthesis of ["(", ")"]) {
+      if (parenthesis === "(" && openCount >= n) {
+        continue;
+      }
+      if (parenthesis === ")" && closeCount >= openCount) {
+        continue;
+      }
 
-  for (let end = start + 1; end <= s.length; end++) {
-    const prefix = s.substring(start, end);
+      path.push(parenthesis);
+      if (parenthesis === "(") {
+        openCount++;
+      } else {
+        closeCount++;
+      }
 
-    if (isPalindrome(prefix)) {
-      path.push(prefix);
-      dfs(s, end, path, res);
+      dfs(path, openCount, closeCount, res);
+
+      // reverting the state
+      if (parenthesis === "(") {
+        openCount--;
+      } else {
+        closeCount--;
+      }
       path.pop();
     }
-  }
+  };
+
+  const ans = [];
+  dfs([], 0, 0, ans);
+  return ans;
 }
 
-function isPalindrome(word) {
-  let l = 0,
-    r = word.length - 1;
-  while (l < r) {
-    if (word.charAt(l) != word.charAt(r)) return false;
-    l++;
-    r--;
-  }
-  return true;
-}
-
-console.log(partition("aab"));
+console.log(generateParentheses(2));
