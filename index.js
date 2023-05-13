a0e5230c3287586495568200bf8f89194d3d015c
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-13 22:56:27 +0200

    Partitioning A String Into Palindromes

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-// Your code here.
-let size = 8;
-
-let board = "";
-
-for (let y = 0; y < size; y++) {
-  for (let x = 0; x < size; x++) {
-    if ((x + y) % 2 == 0) {
-      board += " ";
-    } else {
-      board += "#";
-    }
-  }
-  board += "\n";
+function partition(s) {
+  let res = [];
+  dfs(s, 0, [], res);
+  return res;
 }
 
-console.log(board);
+function dfs(s, start, path, res) {
+  if (start === s.length) {
+    res.push([...path]);
+    return;
+  }
+
+  for (let end = start + 1; end <= s.length; end++) {
+    const prefix = s.substring(start, end);
+
+    if (isPalindrome(prefix)) {
+      path.push(prefix);
+      dfs(s, end, path, res);
+      path.pop();
+    }
+  }
+}
+
+function isPalindrome(word) {
+  let l = 0,
+    r = word.length - 1;
+  while (l < r) {
+    if (word.charAt(l) != word.charAt(r)) return false;
+    l++;
+    r--;
+  }
+  return true;
+}
+
+console.log(partition("aab"));
