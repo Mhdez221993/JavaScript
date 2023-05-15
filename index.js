57c08a9707e3375e98c2a497fc4115fbcc236049
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-15 12:15:07 +0200

    Number of Ways to Decode a Message with memoization

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 function decodeWays(digits) {
-  return dfs(digits, 0);
+  return dfs(0, digits, {});
 }
 
-function dfs(digits, start) {
-  if (start === digits.length) return 1;
+function dfs(startIndex, digits, memo) {
+  if (startIndex === digits.length) return 1;
+  if (startIndex in memo) return memo[startIndex];
 
-  let ans = 0;
+  let ways = 0;
+  if (digits[startIndex] == "0") return ways;
+  // can't decode string with leading 0
 
-  if (digits[start] === "0") return ans;
-
-  ans += dfs(digits, start + 1);
-
+  // decode one digit
+  ways += dfs(startIndex + 1, digits, memo);
+  // decode two digits
   if (
-    start + 2 <= digits.length &&
-    parseInt(digits.slice(start, start + 2)) <= 26
+    startIndex + 2 <= digits.length &&
+    parseInt(digits.substring(startIndex, startIndex + 2)) <= 26
   ) {
-    ans += dfs(digits, start + 2);
+    ways += dfs(startIndex + 2, digits, memo);
   }
 
-  return ans;
+  memo[startIndex] = ways;
+
+  return ways;
 }
