b0da3b00c07deb4727168571352eccbc95a19d3e
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-15 15:41:15 +0200

    Implemnet my filter function

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-function decodeWays(digits) {
-  return dfs(0, digits, {});
-}
-
-function dfs(startIndex, digits, memo) {
-  if (startIndex === digits.length) return 1;
-  if (startIndex in memo) return memo[startIndex];
-
-  let ways = 0;
-  if (digits[startIndex] == "0") return ways;
-  // can't decode string with leading 0
-
-  // decode one digit
-  ways += dfs(startIndex + 1, digits, memo);
-  // decode two digits
-  if (
-    startIndex + 2 <= digits.length &&
-    parseInt(digits.substring(startIndex, startIndex + 2)) <= 26
-  ) {
-    ways += dfs(startIndex + 2, digits, memo);
+Array.prototype.myFilter = function (callbackFn, thisArg) {
+  let newArr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callbackFn(this[i])) {
+      newArr.push(this[i]);
+    }
   }
 
-  memo[startIndex] = ways;
+  return newArr;
+};
 
-  return ways;
-}
+console.log([1, 2, 3, 4].myFilter((value) => value < 3));
