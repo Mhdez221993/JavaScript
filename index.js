41318ee9b2e31c39ab18e69a29367c9d605fb2d4
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-15 09:54:25 +0200

    Word Break with memoization

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 function wordBreak(s, words) {
-  return dfs(s, words, 0);
+  return dfs(s, words, 0, {});
 }
 
-function dfs(s, words, index) {
+function dfs(s, words, index, memo) {
   if (index === s.length) return true;
 
-  let ans = false;
+  if (index in memo) return memo[index];
 
+  let ans = false;
   for (let word of words) {
     if (s.slice(index).startsWith(word)) {
-      ans = ans || dfs(s, words, index + word.length);
+      if (dfs(s, words, index + word.length, memo)) {
+        ans = true;
+        break;
+      }
     }
   }
+
+  memo[index] = ans;
 
   return ans;
 }
 
-let s = "algomonster";
-let words = ["algo", "monster"];
+let s =
+  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
+let words = [
+  "a",
+  "aa",
+  "aaa",
+  "aaaa",
+  "aaaaa",
+  "aaaaaa",
+  "aaaaaaa",
+  "aaaaaaaa",
+  "aaaaaaaaa",
+  "aaaaaaaaaa",
+];
 
 console.log(wordBreak(s, words));
