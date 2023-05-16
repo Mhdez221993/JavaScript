95234aa23e8b6f06f1412e3f8ba1fa271b20b790
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-16 10:54:53 +0200

    Return unique combinations of array elements summing to target

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-var isPalindrome = function (s) {
-  let left = 0;
-  let right = s.length - 1;
+function combinationSum(candidates, target) {
+  candidates.sort((a, b) => a - b);
+  let res = [];
+  dfs(candidates, target, res, [], 0);
+  return res;
+}
 
-  while (left < right) {
-    if (!isAlphanumeric(s[left])) {
-      left++;
-    } else if (!isAlphanumeric(s[right])) {
-      right--;
-    } else {
-      if (s[left].toLowerCase() !== s[right].toLowerCase()) {
-        return false;
-      }
-      left++;
-      right--;
-    }
+function dfs(nums, target, res, path, start) {
+  if (target === 0) {
+    res.push([...path]);
+    return;
   }
 
-  return true;
-};
+  for (let i = start; i < nums.length; i++) {
+    if (target - nums[i] < 0) continue;
 
-const isAlphanumeric = function (char) {
-  return /^[a-z0-9]+$/i.test(char);
-};
+    path.push(nums[i]);
+
+    dfs(nums, target - nums[i], res, path, i);
+
+    path.pop();
+  }
+}
+
+let candidates = [2, 3, 6, 7];
+let target = 7;
+console.log(combinationSum(candidates, target));
