ab1b6714dfede0a97da08c9a2c5a3fe0fe2af52e
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-27 01:59:37 +0200

    Find Minimum in Rotated Sorted Array

--- index.js
+++ index.js
@@ -1,40 +1,18 @@
-var search = function (nums, target) {
+var findMin = function (nums) {
   let left = 0;
   let right = nums.length - 1;
 
-  while (left <= right) {
+  while (left < right) {
     let mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return true;
-    if (nums[left] === nums[mid]) {
-      left++;
-      continue;
-    }
 
-    if (nums[left] < nums[mid]) {
-      if (nums[left] <= target && target < nums[mid]) {
-        right = mid - 1;
-      } else {
-        left = mid + 1;
-      }
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
     } else {
-      if (nums[mid] < target && target <= nums[right]) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
+      right = mid;
     }
   }
 
-  return false;
+  return nums[left];
 };
 
-console.log(
-  search([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1], 2)
-);
-
-/*
-  | |
-[1, 0, 1, 1, 1]
-target = 0
-
-*/
+console.log(findMin([3, 1, 2]));
