699945fbac413f560533e68cbb215469adffa3af
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-27 01:28:03 +0200

    Search in Rotated Sorted Array II

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-var searchMatrix = function (matrix, target) {
+var search = function (nums, target) {
   let left = 0;
-  let right = matrix.length - 1;
+  let right = nums.length - 1;
 
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
-    if (matrix[mid][0] > target) {
-      right = mid - 1;
-    } else {
-      left = mid + 1;
+    if (nums[mid] === target) return true;
+    if (nums[left] === nums[mid]) {
+      left++;
+      continue;
     }
-  }
 
-  let row = right;
-  if (row < 0) row = 0;
-
-  left = 0;
-  right = matrix[row].length - 1;
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-    if (matrix[row][mid] === target) {
-      return true;
-    } else if (matrix[row][mid] > target) {
-      right = mid - 1;
+    if (nums[left] < nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
     } else {
-      left = mid + 1;
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
     }
   }
 
   return false;
 };
 
-console.log(searchMatrix([[1], [3], [5]], 3));
+console.log(
+  search([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1], 2)
+);
+
+/*
+  | |
+[1, 0, 1, 1, 1]
+target = 0
+
+*/
