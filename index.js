6026a3996cb17cfd555832b1146ea651a98a81f4
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-27 00:34:00 +0200

    Search a 2D Matrix

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var searchInsert = function (nums, target) {
+var searchMatrix = function (matrix, target) {
   let left = 0;
-  let right = nums.length - 1;
+  let right = matrix.length - 1;
 
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
-
-    if (nums[mid] === target) {
-      return mid;
-    } else if (nums[mid] > target) {
+    if (matrix[mid][0] > target) {
       right = mid - 1;
     } else {
       left = mid + 1;
     }
   }
 
-  return left;
+  let row = right;
+  if (row < 0) row = 0;
+
+  left = 0;
+  right = matrix[row].length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (matrix[row][mid] === target) {
+      return true;
+    } else if (matrix[row][mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return false;
 };
 
-console.log(searchInsert([1, 3, 5, 6], 5));
+console.log(searchMatrix([[1], [3], [5]], 3));
