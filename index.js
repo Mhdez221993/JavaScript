043a6e254ea43bde42649608e655bb567b411ff8
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-26 22:26:14 +0200

    Search for targe in a sorted array

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-var findMedianSortedArrays = function (nums1, nums2) {
-  if (nums1.length > nums2.length) {
-    [nums1, nums2] = [nums2, nums1];
-  }
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
 
-  let len1 = nums1.length;
-  let len2 = nums2.length;
-  let start = 0;
-  let end = len1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    }
 
-  while (start <= end) {
-    let mid1 = (start + end) >> 1;
-    let mid2 = ((len1 + len2 + 1) >> 1) - mid1;
-
-    let max1 = mid1 === 0 ? -Infinity : nums1[mid1 - 1];
-    let min1 = mid1 === len1 ? Infinity : nums1[mid1];
-
-    let max2 = mid2 === 0 ? -Infinity : nums2[mid2 - 1];
-    let min2 = mid2 === len2 ? Infinity : nums2[mid2];
-
-    if (max1 <= min2 && max2 <= min1) {
-      if ((len1 + len2) % 2 === 0) {
-        return (Math.max(max1, max2) + Math.min(min1, min2)) / 2.0;
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
       } else {
-        return Math.max(max1, max2);
+        left = mid + 1;
       }
-    } else if (max1 > min2) {
-      end = mid1 - 1;
     } else {
-      start = mid1 + 1;
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
     }
   }
+
+  return -1;
 };
+
+console.log(search([4, 5, 6, 7, 0, 1, 2], 0));
+console.log(search([3, 5, 1], 3));
+console.log(search([1, 3], 3));
