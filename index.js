1f7a337ad5ebf31eea03a8173ff38c7fd8d8ddac
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-26 22:31:51 +0200

    Search Insert Position

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-var search = function (nums, target) {
+var searchInsert = function (nums, target) {
   let left = 0;
   let right = nums.length - 1;
 
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
+
     if (nums[mid] === target) {
       return mid;
-    }
-
-    if (nums[left] <= nums[mid]) {
-      if (nums[left] <= target && target < nums[mid]) {
-        right = mid - 1;
-      } else {
-        left = mid + 1;
-      }
+    } else if (nums[mid] > target) {
+      right = mid - 1;
     } else {
-      if (nums[mid] < target && target <= nums[right]) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
+      left = mid + 1;
     }
   }
 
-  return -1;
+  return left;
 };
 
-console.log(search([4, 5, 6, 7, 0, 1, 2], 0));
-console.log(search([3, 5, 1], 3));
-console.log(search([1, 3], 3));
+console.log(searchInsert([1, 3, 5, 6], 5));
