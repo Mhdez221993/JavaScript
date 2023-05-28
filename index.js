096a9667b4da003d06f571fcd2bc228b4f4b5acf
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-29 00:14:06 +0200

    Find the min Depth on a tree

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-var isSymmetric = function (root) {
-  function isMirror(root1, root2) {
-    if (root1 == null && root2 == null) return true;
-    if (root1 == null || root2 == null) return false;
-    if (root1.val !== root2.val) return false;
-    return (
-      isMirror(root1.right, root2.left) && isMirror(root1.left, root2.right)
-    );
-  }
-  return isMirror(root, root);
+var minDepth = function (root) {
+  if (!root) return 0;
+
+  let left = minDepth(root.left);
+  let right = minDepth(root.right);
+
+  return left === 0 || right === 0
+    ? left + right + 1
+    : Math.min(left, right) + 1;
 };
