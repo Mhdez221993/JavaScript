e859f6f4c31e406f2d8388c78081270cc9d20299
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-28 13:25:09 +0200

    Implement BST find function

--- index.js
+++ index.js
@@ -1,52 +1,29 @@
-function serialize(root) {
-  let res = [];
-  serialize_dfs(root, res);
-  return res.join(" ");
-}
-
-function serialize_dfs(root, res) {
-  if (!root) {
-    res.push("x");
-    return;
-  }
-  res.push(root.val);
-  serialize_dfs(root.left, res);
-  serialize_dfs(root.right, res);
-}
-
-function deserialize(s) {
-  // create an iterator that returns a token each time we call `next`
-  return deserialize_dfs(s.split(" ")[Symbol.iterator]());
-}
-
-function deserialize_dfs(nodes) {
-  let val = nodes.next().value;
-  if (val === "x") return;
-  const cur = new Node(parseInt(val, 10));
-  cur.left = deserialize_dfs(nodes);
-  cur.right = deserialize_dfs(nodes);
-  return cur;
+function find(tree, val) {
+  if (!tree) return false;
+  if (tree.val === val) return true;
+  else if (tree.val > val) return find(tree.left, val);
+  else return find(tree.right, val);
 }
 
 let tree = {
-  value: 5,
+  val: 5,
   left: {
-    value: 4,
+    val: 4,
     left: {
-      value: 3,
+      val: 3,
       left: null,
       right: null,
     },
     right: null,
   },
   right: {
-    value: 6,
+    val: 6,
     right: {
-      value: 8,
+      val: 8,
       left: null,
       right: null,
     },
   },
 };
 
-console.log(treeMaxDepth(tree));
+console.log(find(tree, 22));
