bf3038446efb86755fd63960134158a2758d76f9
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-29 19:08:06 +0200

    Flatten Binary Tree to Linked List

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
-var minDepth = function (root) {
-  if (!root) return 0;
-
-  let left = minDepth(root.left);
-  let right = minDepth(root.right);
-
-  return left === 0 || right === 0
-    ? left + right + 1
-    : Math.min(left, right) + 1;
+const bt = {
+  val: 1,
+  left: {
+    val: 2,
+    left: {
+      val: 3,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 4,
+      left: null,
+      right: null,
+    },
+  },
+  right: {
+    val: 5,
+    left: null,
+    right: {
+      val: 6,
+      left: null,
+      right: null,
+    },
+  },
 };
+
+var flatten = function (root) {
+  if (root == null) return [];
+
+  function dfs(root) {
+    if (root === null) return;
+
+    dfs(root.left);
+    dfs(root.right);
+
+    if (root.left !== null) {
+      const temp = root.right;
+      root.right = root.left;
+      root.left = null;
+
+      let t = root.right;
+      while (t.right !== null) {
+        t = t.right;
+      }
+      t.right = temp;
+    }
+  }
+
+  dfs(root);
+
+  return root;
+};
+
+flatten(bt);
+console.log(bt);
