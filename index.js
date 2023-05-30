cfcff61dcb905076d198e76e93d8b802d7e149f8
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-30 18:59:04 +0200

    capture all regions that are 4-directionally surrounded by 'X'

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
-const bt = {
-  val: 1,
-  left: {
-    val: 2,
-    left: null,
-    right: null,
-  },
-  right: {
-    val: 3,
-    left: null,
-    right: null,
-  },
-};
+var solve = function (board) {
+  if (!board || board.length === 0) return;
+  const rows = board.length;
+  const cols = board[0].length;
 
-var sumNumbers = function (root) {
-  function dfs(root, currSum) {
-    if (root == null) return 0;
+  // Create a DFS function to handle traversal
+  const dfs = (i, j) => {
+    if (i < 0 || j < 0 || i >= rows || j >= cols || board[i][j] !== "O") return;
+    board[i][j] = "T";
 
-    currSum = currSum * 10 + root.val;
+    dfs(i - 1, j); // up
+    dfs(i + 1, j); // down
+    dfs(i, j - 1); // left
+    dfs(i, j + 1); // right
+  };
 
-    if (root.left == null && root.right == null) return currSum;
-
-    return dfs(root.left, currSum) + dfs(root.right, currSum);
+  // Check first and last column
+  for (let i = 0; i < rows; i++) {
+    if (board[i][0] === "O") dfs(i, 0);
+    if (board[i][cols - 1] === "O") dfs(i, cols - 1);
   }
 
-  return dfs(root, 0);
+  // Check first row and last row
+  for (let j = 0; j < cols; j++) {
+    if (board[0][j] === "O") dfs(0, j);
+    if (board[rows - 1][j] === "O") dfs(rows - 1, j);
+  }
+
+  // Post-process the board
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (board[i][j] === "O") board[i][j] = "X";
+      else if (board[i][j] === "T") board[i][j] = "O";
+    }
+  }
+
+  return board;
 };
 
-console.log(sumNumbers(bt));
+let board = [
+  ["X", "X", "X", "X"],
+  ["X", "O", "O", "X"],
+  ["X", "X", "O", "X"],
+  ["X", "O", "X", "X"],
+];
+console.log(solve(board));
