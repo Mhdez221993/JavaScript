e410dfb12875f9450dbc0296fee13e82840e6ef4
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-30 23:30:51 +0200

    My Calendar I

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-var solution = function (isBadVersion) {
-  return function (n) {
-    let left = 1;
-    let right = n;
-    while (left < right) {
-      let mid = Math.floor((left + right) / 2);
-      if (isBadVersion(mid)) right = mid;
-      else left = mid + 1;
+MyCalendar.prototype.book = function (start, end) {
+  let left = 0;
+  let right = this.calendar.length - 1;
+  let indx = this.calendar.length;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (this.calendar[mid][0] > start) {
+      indx = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
     }
-    return right;
-  };
+  }
+
+  if (
+    (indx > 0 && this.calendar[indx - 1][1] > start) ||
+    (indx < this.calendar.length && this.calendar[indx][0] < end)
+  ) {
+    return false;
+  }
+
+  this.calendar.splice(indx, 0, [start, end]);
+
+  return true;
 };
