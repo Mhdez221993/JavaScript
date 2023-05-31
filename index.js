3a15081245b5c83e2ac89c0bcbd4e0fbcce632db
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-31 13:06:23 +0200

    Binary tree path

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
+function printPath(root) {
+  let res = [];
+  if (root) dfs(root, [], res);
+  return res;
+}
+
 function dfs(root, path, res) {
-  // exit condition, reached leaf node, append paths to results
-  if (root.children.length === 0) {
+  if (root == null) return;
+
+  if (root.left == null && root.right == null) {
     path.push(root.val);
     res.push(path.join("->"));
     path.pop();
     return;
   }
-  // dfs on each non-null child
-  for (const child of root.children) {
-    if (child) {
-      path.push(root.val);
-      dfs(child, path, res);
-      path.pop();
-    }
-  }
+
+  path.push(root.val);
+  dfs(root.left, path, res);
+  dfs(root.right, path, res);
+  path.pop();
 }
 
-function ternaryTreePaths(root) {
-  let res = [];
-  if (root) dfs(root, [], res);
-  return res;
-}
+var bt = {
+  val: 1,
+  left: {
+    val: 2,
+    left: {
+      val: 4,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 5,
+      left: null,
+      right: null,
+    },
+  },
+  right: {
+    val: 3,
+    left: {
+      val: 6,
+      left: null,
+      right: null,
+    },
+    right: null,
+  },
+};
+
+console.log(printPath(bt));
