46894576ad0ca9cbf2ca86b0f3021e51ecb1794c
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-31 13:14:59 +0200

    Combinatorial Example Problem

--- index.js
+++ index.js
@@ -1,49 +1,17 @@
-function printPath(root) {
-  let res = [];
-  if (root) dfs(root, [], res);
-  return res;
-}
-
-function dfs(root, path, res) {
-  if (root == null) return;
-
-  if (root.left == null && root.right == null) {
-    path.push(root.val);
-    res.push(path.join("->"));
-    path.pop();
+function dfs(n, res, startIndex, path) {
+  if (startIndex == n) {
+    res.push(path.join(""));
     return;
   }
-
-  path.push(root.val);
-  dfs(root.left, path, res);
-  dfs(root.right, path, res);
-  path.pop();
+  ["a", "b"].forEach((letter) => {
+    path.push(letter);
+    dfs(n, res, startIndex + 1, path);
+    path.pop();
+  });
 }
 
-var bt = {
-  val: 1,
-  left: {
-    val: 2,
-    left: {
-      val: 4,
-      left: null,
-      right: null,
-    },
-    right: {
-      val: 5,
-      left: null,
-      right: null,
-    },
-  },
-  right: {
-    val: 3,
-    left: {
-      val: 6,
-      left: null,
-      right: null,
-    },
-    right: null,
-  },
-};
-
-console.log(printPath(bt));
+function letterCombination(n) {
+  var res = [];
+  dfs(n, res, 0, []);
+  return res;
+}
