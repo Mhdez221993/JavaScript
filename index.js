f096d34c66f830f81cf67bceb0caf7324ba70d95
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-31 19:46:13 +0200

    Minimum Number of Coins to Make Up a Given Value

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-function decodeWays(digits) {
-  return dfs(0, digits, {});
-}
+function minCoins(coins, amount, sum, memo) {
+  if (sum === amount) return 0;
+  if (sum > amount) return Infinity;
+  if (memo[sum] != -1) return memo[sum];
 
-function dfs(startIndex, digits, memo) {
-  if (startIndex === digits.length) return 1;
-  if (startIndex in memo) return memo[startIndex];
-
-  let ways = 0;
-  if (digits[startIndex] == "0") return ways;
-  // can't decode string with leading 0
-
-  // decode one digit
-  ways += dfs(startIndex + 1, digits, memo);
-  // decode two digits
-  if (
-    startIndex + 2 <= digits.length &&
-    parseInt(digits.substring(startIndex, startIndex + 2)) <= 26
-  ) {
-    ways += dfs(startIndex + 2, digits, memo);
+  let ans = Infinity;
+  for (let coin of coins) {
+    let result = minCoins(coins, amount, sum + coin, memo);
+    if (result === Infinity) continue;
+    ans = Math.min(ans, result + 1);
   }
 
-  memo[startIndex] = ways;
+  return (memo[sum] = ans);
+}
 
-  return ways;
+function coinChange(coins, amount) {
+  let memo = new Array(amount + 1).fill(-1);
+  let result = minCoins(coins, amount, 0, memo);
+  return result === Infinity ? -1 : result;
 }
