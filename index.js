7ada095a95b7f9a4ac2570b9d47c83eb4d8a9268
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-31 14:21:14 +0200

    Generate All Valid Parentheses

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-function dfs(n, res, startIndex, path) {
-  if (startIndex == n) {
-    res.push(path.join(""));
-    return;
+function generateParentheses(n) {
+  function dfs(startIndex, path, openCount, closeCount, res) {
+    if (startIndex == 2 * n) {
+      // make a deep copy since otherwise we'd be append the same list over and over
+      res.push(path.join(""));
+      return;
+    }
+    if (openCount < n) {
+      path.push("(");
+      dfs(startIndex + 1, path, openCount + 1, closeCount, res);
+      path.pop();
+    }
+    if (closeCount < openCount) {
+      path.push(")");
+      dfs(startIndex + 1, path, openCount, closeCount + 1, res);
+      path.pop();
+    }
   }
-  ["a", "b"].forEach((letter) => {
-    path.push(letter);
-    dfs(n, res, startIndex + 1, path);
-    path.pop();
-  });
-}
 
-function letterCombination(n) {
-  var res = [];
-  dfs(n, res, 0, []);
+  const res = [];
+  dfs(0, [], 0, 0, res);
   return res;
 }
