bfa68cbe529a88c5b00ce6dd3b6b05dd60820492
Moises Hernandez <mhdezcoronado@gmail.com>
2023-05-31 17:38:43 +0200

    Number of Ways to Decode a Message

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-function fib(n) {
-  if (n === 0 || n === 1) {
-    return n;
+function decodeWays(digits) {
+  return dfs(0, digits, {});
+}
+
+function dfs(startIndex, digits, memo) {
+  if (startIndex === digits.length) return 1;
+  if (startIndex in memo) return memo[startIndex];
+
+  let ways = 0;
+  if (digits[startIndex] == "0") return ways;
+  // can't decode string with leading 0
+
+  // decode one digit
+  ways += dfs(startIndex + 1, digits, memo);
+  // decode two digits
+  if (
+    startIndex + 2 <= digits.length &&
+    parseInt(digits.substring(startIndex, startIndex + 2)) <= 26
+  ) {
+    ways += dfs(startIndex + 2, digits, memo);
   }
-  return fib(n - 1) + fib(n - 2);
+
+  memo[startIndex] = ways;
+
+  return ways;
 }
-
-function fibWithMemoization(n, memo) {
-  // check in memo, if found, retrieve and return right away
-  if (n in memo) return memo[n];
-
-  if (n === 0 || n === 1) return n;
-
-  const res = fib(n - 1, memo) + fib(n - 2, memo);
-
-  // save result to memo before returning
-  memo[n] = res;
-  return res;
-}
-
-console.log(fib(12));
