aa400f1b096b6c9a5802367b6ac4e34ccb6a3d62
Moises Hernandez <mhdezcoronado@gmail.com>
2023-06-02 13:19:04 +0200

    DFS on Grphs

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-var grayCode = function (n) {
-  let length = 1 << n;
-  let visited = new Array(length).fill(false);
-
-  function dfs(start_index, code) {
-    if (start_index === length) {
-      return true;
-    }
-    for (let i = 0; i < n; i++) {
-      let new_code = code ^ (1 << i);
-      if (!visited[new_code]) {
-        path.push(new_code);
-        visited[new_code] = true;
-        if (dfs(start_index + 1, new_code)) return true;
-        visited[new_code] = false;
-        path.pop();
-      }
-    }
-    return false;
+function dfs(root, visited) {
+  for (const neighbor of get_neighbors(root)) {
+    if (visited.has(neighbor)) continue;
+    visited.add(neighbor);
+    dfs(neighbor, visited);
   }
-
-  let path = [0];
-  visited[0] = true;
-  dfs(1, 0);
-  return path;
-};
+}
