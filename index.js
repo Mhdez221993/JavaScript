197dff648d5447a8289521fd8bc7beb61caca724
Moises Hernandez <mhdezcoronado@gmail.com>
2023-06-02 13:37:36 +0200

    Traverse a N-tree BFS

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
-function dfs(root, visited) {
-  for (const neighbor of get_neighbors(root)) {
-    if (visited.has(neighbor)) continue;
-    visited.add(neighbor);
-    dfs(neighbor, visited);
+function printTree(tree) {
+  if (!tree) return [];
+  let res = [];
+  let queue = [tree];
+
+  while (queue.length > 0) {
+    let node = queue.shift();
+    res.push(node.value);
+
+    for (let child of node.children) {
+      queue.push(child);
+    }
   }
+
+  return res;
 }
+
+let tree = {
+  value: 1,
+  children: [
+    {
+      value: 2,
+      children: [
+        {
+          value: 4,
+          children: [],
+        },
+        {
+          value: 5,
+          children: [],
+        },
+      ],
+    },
+    {
+      value: 3,
+      children: [
+        {
+          value: 6,
+          children: [],
+        },
+      ],
+    },
+  ],
+};
+
+console.log(printTree(tree));
