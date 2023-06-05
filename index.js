7aa4b1e099ac1f31affc5deded292af83aa99868
Moises Hernandez <mhdezcoronado@gmail.com>
2023-06-05 13:03:29 +0200

    Clone Graph DFS

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-function Node(val, neighbors) {
-  this.val = val === undefined ? 0 : val;
-  this.neighbors = neighbors === undefined ? [] : neighbors;
-}
+var cloneGraphDFS = function (node, map = new Map()) {
+  if (!node) return null;
 
-var cloneGraph = function (node) {
-  if (!node) return node;
+  if (map.has(node)) return map.get(node);
 
-  let map = new Map();
-  let queue = [node];
-  map.set(node, new Node(node.val, []));
+  let cloneNode = new Node(node.val, []);
 
-  while (queue.length > 0) {
-    let currNode = queue.shift();
-    for (let neighbor of currNode.neighbors) {
-      if (!map.get(neighbor)) {
-        map.set(neighbor, new Node(neighbor.val, []));
-        queue.push(neighbor);
-      }
+  map.set(node, cloneNode);
 
-      map.get(currNode).neighbors.push(map.get(neighbor));
-    }
+  for (let neighbor of node.neighbors) {
+    cloneNode.neighbors.push(cloneGraph(neighbor, map));
   }
 
-  return map.get(node);
+  return cloneNode;
 };
