fc84539d0122d0026ff56b3872449e831d6c3768
Moises Hernandez <mhdezcoronado@gmail.com>
2023-06-05 21:23:39 +0200

    Course schedule DFS

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
-var numIslands = function (grid) {
-  let count = 0;
-  let rows = grid.length;
-  let cols = grid[0].length;
+var canFinish = function (numCourses, prerequisites) {
+  let graph = new Array(numCourses).fill(0).map(() => []);
+  let visited = new Array(numCourses).fill(0);
 
-  function bfs(row, col) {
-    let queue = [[row, col]];
-    while (queue.length > 0) {
-      let [i, j] = queue.shift();
-
-      // check i and j are in boudary and curr value === 0
-      if (i >= 0 && j >= 0 && i < rows && j < cols && grid[i][j] === "1") {
-        // mark the cell as visited
-        grid[i][j] = "0";
-
-        // for every 1 we encounter push all adjacent cells: up, down, left, right directions
-        queue.push([i - 1, j]); // up
-        queue.push([i + 1, j]); // down
-        queue.push([i, j - 1]); // left
-        queue.push([i, j + 1]); // right
-      }
-    }
+  for (let i = 0; i < prerequisites.length; i++) {
+    let [course, prerequisite] = prerequisites[i];
+    graph[course].push(prerequisite);
   }
 
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (grid[i][j] === "1") {
-        bfs(i, j);
-        count += 1;
-      }
+  function hasCycle(i) {
+    if (visited[i] === -1) return true;
+    if (visited[i] === 1) return false;
+
+    visited[i] = -1;
+
+    for (let j = 0; j < graph[i].length; j++) {
+      if (hasCycle(graph[i][j])) return true;
     }
+
+    visited[i] = 1;
   }
 
-  return count;
+  for (let i = 0; i < numCourses; i++) {
+    if (hasCycle(i)) return false;
+  }
+
+  return true;
 };
 
-let grid = [
-  ["1", "1", "0", "0", "0"],
-  ["1", "1", "0", "0", "0"],
-  ["0", "0", "1", "0", "0"],
-  ["0", "0", "0", "1", "1"],
-];
-
-console.log(numIslands(grid));
+console.log(canFinish(2, [[1, 0]]));
