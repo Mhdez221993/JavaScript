ec99dbfb1a903f18274a2e7848903901fd5c6a7b
Moises Hernandez <mhdezcoronado@gmail.com>
2023-06-05 20:18:25 +0200

    Number of Islands DFS

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-var rightSideView = function (root) {
-  if (!root) return [];
+var numIslands = function (grid) {
+  let count = 0;
+  let rows = grid.length;
+  let cols = grid[0].length;
 
-  let res = [];
-  let queue = [root];
+  function dfs(grid, i, j) {
+    if (i < 0 || j < 0 || i >= rows || j >= cols || grid[i][j] == 0) return;
+    // mark the cell as visited
+    grid[i][j] = "0";
 
-  while (queue.length > 0) {
-    let n = queue.length;
-    for (let i = 0; i < n; i++) {
-      let node = queue.shift();
-      if (i === n - 1) res.push(node.val);
+    // visti all adjacent cells in up, down, left, right directions
+    dfs(grid, i - 1, j); // up
+    dfs(grid, i + 1, j); // down
+    dfs(grid, i, j - 1); // left
+    dfs(grid, i, j + 1); // right
+  }
 
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] == 1) {
+        dfs(grid, i, j);
+        count += 1;
+      }
     }
   }
 
-  return res;
+  return count;
 };
+
+let grid = [
+  ["1", "1", "0", "0", "0"],
+  ["1", "1", "0", "0", "0"],
+  ["0", "0", "1", "0", "0"],
+  ["0", "0", "0", "1", "1"],
+];
+
+console.log(numIslands(grid));
