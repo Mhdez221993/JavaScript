4bef4da529df34a4f5fa8244df1f7c270fa00368
Moises Hernandez <mhdezcoronado@gmail.com>
2023-06-07 19:55:07 +0200

    Open the Lock BFS

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-const directions = [
-  [0, 1],
-  [1, 0],
-  [0, -1],
-  [-1, 0],
-];
-
-function mapGateDistances(dungeonMap) {
-  const queue = [];
-  const n = dungeonMap.length,
-    m = dungeonMap[0].length;
-  for (let row = 0; row < n; row++) {
-    for (let col = 0; col < m; col++) {
-      if (dungeonMap[row][col] == 0) {
-        queue.push([row, col]);
-      }
-    }
-  }
-
-  while (queue.length > 0) {
-    let [row, col] = queue.shift();
-    for (const [deltaRow, deltaCol] of directions) {
-      const totalRow = row + deltaRow;
-      const totalCol = col + deltaCol;
-      if (totalRow >= 0 && totalRow < n && totalCol >= 0 && totalCol < m) {
-        if (dungeonMap[totalRow][totalCol] == INF) {
-          dungeonMap[totalRow][totalCol] = dungeonMap[row][col] + 1;
-          queue.push([totalRow, totalCol]);
-        }
-      }
-    }
-  }
-  return dungeonMap;
+const nextDigit = new Map();
+for (let i = 0; i < 10; i++) {
+  let val = (i + 1) % 10;
+  nextDigit.set(i.toString(), val.toString());
+}
+const prevDigit = new Map();
+for (let [key, val] of nextDigit.entries()) {
+  prevDigit.set(val, key);
 }
 
-const INF = 2147483647;
+function numSteps(targetCombo, trappedCombos) {
+  if (targetCombo === "0000") return 0;
+  const trappedComboSet = new Set([...trappedCombos]);
+  const steps = new Map([["0000", 0]]);
+  const queue = ["0000"];
 
-let dungeon_map = [
-  [INF, -1, 0, INF],
-  [INF, INF, INF, -1],
-  [INF, -1, INF, -1],
-  [0, -1, INF, INF],
-];
+  while (queue.length > 0) {
+    let top = queue.shift();
 
-console.log(mapGateDistances(dungeon_map));
+    for (let i = 0; i < 4; i++) {
+      let newCombo =
+        top.slice(0, i) + nextDigit.get(top.charAt(i)) + top.slice(i + 1);
+
+      if (!trappedComboSet.has(newCombo) && !steps.has(newCombo)) {
+        queue.push(newCombo);
+        steps.set(newCombo, steps.get(top) + 1);
+        if (newCombo === targetCombo) return steps.get(newCombo);
+      }
+
+      newCombo =
+        top.slice(0, i) + prevDigit.get(top.charAt(i)) + top.slice(i + 1);
+
+      if (!trappedComboSet.has(newCombo) && !steps.has(newCombo)) {
+        queue.push(newCombo);
+        steps.set(newCombo, steps.get(top) + 1);
+        if (newCombo === targetCombo) return steps.get(newCombo);
+      }
+    }
+  }
+  return -1;
+}
+
+let target_combo = "0202";
+let trapped_combos = ["0201", "0101", "0102", "1212", "2002"];
+console.log(numSteps(target_combo, trapped_combos));
