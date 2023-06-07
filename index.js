9ba27b66fe9b36211f6cc1ba4566354787c9f99c
Moises Hernandez <mhdezcoronado@gmail.com>
2023-06-07 20:42:55 +0200

    Word ledder BFS

--- index.js
+++ index.js
@@ -1,45 +1,50 @@
-const nextDigit = new Map();
-for (let i = 0; i < 10; i++) {
-  let val = (i + 1) % 10;
-  nextDigit.set(i.toString(), val.toString());
-}
-const prevDigit = new Map();
-for (let [key, val] of nextDigit.entries()) {
-  prevDigit.set(val, key);
-}
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-function numSteps(targetCombo, trappedCombos) {
-  if (targetCombo === "0000") return 0;
-  const trappedComboSet = new Set([...trappedCombos]);
-  const steps = new Map([["0000", 0]]);
-  const queue = ["0000"];
-
-  while (queue.length > 0) {
-    let top = queue.shift();
-
-    for (let i = 0; i < 4; i++) {
-      let newCombo =
-        top.slice(0, i) + nextDigit.get(top.charAt(i)) + top.slice(i + 1);
-
-      if (!trappedComboSet.has(newCombo) && !steps.has(newCombo)) {
-        queue.push(newCombo);
-        steps.set(newCombo, steps.get(top) + 1);
-        if (newCombo === targetCombo) return steps.get(newCombo);
+function getNeighbors(word, unvisitedWords) {
+  unvisitedNeighbors = [];
+  for (let j = 0; j < word.length; j++) {
+    for (const letter of alphabet) {
+      const w = (word.slice(0, j) + letter + word.slice(j + 1)).toUpperCase();
+      if (unvisitedWords.has(w)) {
+        unvisitedNeighbors.push(w);
+        unvisitedWords.delete(w); // mark next_word as visited
       }
+    }
+  }
+  return unvisitedNeighbors;
+}
 
-      newCombo =
-        top.slice(0, i) + prevDigit.get(top.charAt(i)) + top.slice(i + 1);
-
-      if (!trappedComboSet.has(newCombo) && !steps.has(newCombo)) {
-        queue.push(newCombo);
-        steps.set(newCombo, steps.get(top) + 1);
-        if (newCombo === targetCombo) return steps.get(newCombo);
+function wordLadder(begin, end, wordList) {
+  if (begin == end) return 0;
+  const unvisitedWords = new Set(wordList); // make a set because existence query is O(1) vs O(N) for list
+  const queue = [begin];
+  unvisitedWords.delete(begin);
+  let distance = 0;
+  while (queue.length > 0) {
+    const n = queue.length;
+    distance++;
+    for (let i = 0; i < n; i++) {
+      const word = queue.shift();
+      for (const w of getNeighbors(word, unvisitedWords)) {
+        if (w == end) return distance;
+        queue.push(w);
       }
     }
   }
   return -1;
 }
 
-let target_combo = "0202";
-let trapped_combos = ["0201", "0101", "0102", "1212", "2002"];
-console.log(numSteps(target_combo, trapped_combos));
+let start = "COLD";
+let end = "WARM";
+let word_list = [
+  "COLD",
+  "GOLD",
+  "CORD",
+  "SOLD",
+  "CARD",
+  "WARD",
+  "WARM",
+  "TARD",
+];
+
+console.log(wordLadder(start, end, word_list));
