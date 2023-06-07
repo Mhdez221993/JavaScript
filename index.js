1f21a2f5e6f6e0ed5472ef8da6a7ecf194c72640
Moises Hernandez <mhdezcoronado@gmail.com>
2023-06-07 16:44:13 +0200

    Walls and Gates / Zombie in Matrix BFS

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
+const directions = [
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+];
+
 function mapGateDistances(dungeonMap) {
-  let rows = dungeonMap.length;
-  let cols = dungeonMap[0].length;
-
-  function dfs(i, j, steps) {
-    if (
-      i < 0 ||
-      j < 0 ||
-      i > rows - 1 ||
-      j > cols - 1 ||
-      dungeonMap[i][j] === -1 ||
-      dungeonMap[i][j] < steps
-    )
-      return;
-
-    if (dungeonMap[i][j] !== 0) dungeonMap[i][j] = steps;
-    dfs(i - 1, j, steps + 1); // up
-    dfs(i + 1, j, steps + 1); // down
-    dfs(i, j - 1, steps + 1); // left
-    dfs(i, j + 1, steps + 1); // right
-  }
-
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (dungeonMap[i][j] === 0) dfs(i, j, 0);
+  const queue = [];
+  const n = dungeonMap.length,
+    m = dungeonMap[0].length;
+  for (let row = 0; row < n; row++) {
+    for (let col = 0; col < m; col++) {
+      if (dungeonMap[row][col] == 0) {
+        queue.push([row, col]);
+      }
     }
   }
 
+  while (queue.length > 0) {
+    let [row, col] = queue.shift();
+    for (const [deltaRow, deltaCol] of directions) {
+      const totalRow = row + deltaRow;
+      const totalCol = col + deltaCol;
+      if (totalRow >= 0 && totalRow < n && totalCol >= 0 && totalCol < m) {
+        if (dungeonMap[totalRow][totalCol] == INF) {
+          dungeonMap[totalRow][totalCol] = dungeonMap[row][col] + 1;
+          queue.push([totalRow, totalCol]);
+        }
+      }
+    }
+  }
   return dungeonMap;
 }
 
-let INF = Infinity;
+const INF = 2147483647;
 
 let dungeon_map = [
   [INF, -1, 0, INF],
