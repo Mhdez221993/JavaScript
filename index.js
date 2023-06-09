e8c05675eb6a68c18902ff8a222822c1388e4ef2
Moises Hernandez <mhdezcoronado@gmail.com>
2023-06-09 14:41:06 +0200

    Implement priority queue using linked list

--- index.js
+++ index.js
@@ -1,60 +1,67 @@
-class PriorityQueue {
-  constructor() {
-    this.queue = [];
-  }
-
-  // method to add an element to the queue
-  enqueue(item, priority) {
-    const queueElement = { item, priority };
-    if (this.isEmpty()) {
-      // If the queue is empty we can just add the element
-      this.queue.push(queueElement);
-    } else {
-      let added = false;
-      for (let i = 0; i < this.queue.length; i++) {
-        if (queueElement.priority < this.queue[i].priority) {
-          // If the new item's priority is higher, it goes to the right position in the queue
-          this.queue.splice(i, 0, queueElement);
-          added = true;
-          break;
-        }
-      }
-      // If the item's priority is the lowest, it goes to the end of the queue
-      if (!added) {
-        this.queue.push(queueElement);
-      }
-    }
-  }
-
-  // method to remove an element from the queue
-  dequeue() {
-    if (this.isEmpty()) {
-      return "Underflow";
-    } else {
-      return this.queue.shift();
-    }
-  }
-
-  // helper method to check if the queue is empty
-  isEmpty() {
-    return this.queue.length === 0;
-  }
-
-  // helper method to print all elements of the queue
-  print() {
-    var str = "";
-    for (var i = 0; i < this.queue.length; i++) str += this.queue[i].item + " ";
-    return str;
+class Node {
+  constructor(data, priority) {
+    this.data = data;
+    this.priority = priority;
+    this.next = null;
   }
 }
 
-// usage
+class PriorityQueue {
+  constructor() {
+    this.head = null;
+  }
+
+  enqueue(data, priority) {
+    let newNode = new Node(data, priority);
+    // if head is null, make the new node as head
+    if (this.head === null || this.head.priority > newNode.priority) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      // traverse the linked list to find the correct position to insert new node
+      let current = this.head;
+      while (
+        current.next !== null &&
+        current.next.priority < newNode.priority
+      ) {
+        current = current.next;
+      }
+      // insert new node
+      newNode.next = current.next;
+      current.next = newNode;
+    }
+  }
+
+  dequeue() {
+    // underflow condition
+    if (this.head === null) {
+      return null;
+    } else {
+      // remove head and make next node as head
+      let toDequeue = this.head;
+      this.head = this.head.next;
+      return toDequeue.data;
+    }
+  }
+
+  peek() {
+    if (this.head === null) {
+      return null;
+    } else {
+      return this.head.data;
+    }
+  }
+
+  isEmpty() {
+    return this.head === null;
+  }
+}
+
 let pq = new PriorityQueue();
 pq.enqueue("Item 1", 1);
 pq.enqueue("Item 2", 2);
 pq.enqueue("Item 3", 3);
-console.log(pq.print()); // Item 1 Item 2 Item 3
+console.log(pq.dequeue()); // Item 1
 pq.enqueue("Item 0", 0);
-console.log(pq.print()); // Item 0 Item 1 Item 2 Item 3
-pq.dequeue();
-console.log(pq.print()); // Item 1 Item 2 Item 3
+console.log(pq.dequeue()); // Item 0
+console.log(pq.dequeue()); // Item 1
