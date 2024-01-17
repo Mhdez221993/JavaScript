9355af5bf6de972ffc7e03bc5c2bc9973230b543
Moises Hernandez <mhdezcoronado@gmail.com>
2024-01-17 12:34:53 +0100

    Implement a function that perform the insertion sort

---
+++ algo/insertionSort.js
@@ -0,0 +1,18 @@
+// Implement a function that performs an insertion sort
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = key;
+  }
+
+  return arr;
+}
+
+console.log(insertionSort([9, 3, 6, 2, 1, 11]));
