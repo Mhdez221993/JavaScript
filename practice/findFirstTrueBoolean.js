c287d1080b79f2783928ee090ed134a5124c68f8
Moises Hernandez <mhdezcoronado@gmail.com>
2024-01-23 08:31:58 +0100

    Update findBoundary function

--- practice/findFirstTrueBoolean.js
+++ practice/findFirstTrueBoolean.js
@@ -4,18 +4,19 @@
 function findBoundary(arr) {
   let left = 0;
   let right = arr.length - 1;
-  let boundaryIndex = -1;
+  let firstTrueIndex = -1;
 
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
     if (arr[mid]) {
-      boundaryIndex = mid;
+      // feasible here is equal to arr[mid] === true
+      firstTrueIndex = mid;
       right = mid - 1;
     } else {
       left = mid + 1;
     }
   }
-  return boundaryIndex;
+  return firstTrueIndex;
 }
 
 console.log(findBoundary([false, false, true, true, true, true]));
