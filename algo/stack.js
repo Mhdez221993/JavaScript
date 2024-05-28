51dbe21f34eabbc22c62178b5adc6b4669eb5ad4
Moises Hernandez <mhdezcoronado@gmail.com>
2024-05-29 00:05:22 +0200

    Implement a simple stack

--- algo/stack.js
+++ algo/stack.js
@@ -1,89 +1,30 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.prev = null;
-  }
-}
+// Stack support 3 operations
+//   * Insert (or Push)
+//   * Peek (The last item inserted)
+//   * Remove (or pop)
 
 class Stack {
   constructor() {
-    this.top = null;
-    this.count = 0;
+    this.stack = [];
   }
 
-  push(item) {
-    const newNode = new Node(item);
-
-    newNode.prev = this.top;
-    this.top = newNode;
-
-    this.count += 1;
-
-    return this.count;
-  }
-
-  pop() {
-    if (this.isEmpty()) return undefined;
-
-    const node = this.top;
-    this.top = node.prev;
-
-    this.count -= 1;
-
-    return node.val;
-  }
-
-  isEmpty() {
-    return this.count <= 0;
+  insert(item) {
+    this.stack.push(item);
   }
 
   peek() {
-    if (this.isEmpty()) return undefined;
-
-    return this.top.val;
+    return this.stack[this.stack.length - 1];
   }
 
-  length() {
-    return this.count;
+  remove() {
+    return this.stack.pop();
   }
 }
 
-// const stack = new Stack();
-// stack.isEmpty();
-// stack.push(1);
-// stack.push(2);
-// stack.push(3);
-// stack.push(3);
-// stack.pop();
-// console.log(stack.isEmpty());
-
-/* Given a mini program, consisting of a list of stack operations of push, pop, and peek, return the resulting stack after the execution of the program. */
-function execute(program) {
-  const stack = [];
-
-  for (instruction of program) {
-    if (instruction === "peek") {
-      console.log(stack[stack.length - 1]);
-    } else if (instruction === "pop") {
-      stack.pop();
-    } else {
-      const data = parseInt(instruction.substring(5));
-      stack.push(data);
-    }
-  }
-
-  return stack;
-}
-
-const program = [
-  "push 3",
-  "push 7",
-  "push 8",
-  "peek",
-  "pop",
-  "peek",
-  "pop",
-  "pop",
-];
-
-console.log(execute(program));
+const stack = new Stack();
+stack.insert(1);
+stack.insert(2);
+stack.insert(3);
+console.log(stack.peek());
+stack.remove();
+console.log(stack.peek());
